edbc159f27bae3315d9419a7d1053ff34cd7435c
hyeonky <hyeon2762@naver.com>
2024-11-15 17:52:05 +0900

    연습

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,11 @@
 import Image from 'next/image';
+// 함수 선언식
+//  기명 함수
+function func() {}
+
+// 함수호출
+
+const func3 = () => {};
 
 export default function Home() {
   console.log(true ? '참' : '거짓');
@@ -9,9 +16,41 @@ export default function Home() {
     weight: 70,
     height: 180,
   };
-  const array = [1, 2, 3];
+  const array = ['menu1', 'menu2', 'menu3'];
+
+  const users = [
+    { name: '겨울', age: 2 },
+    { name: '가을', age: 3 },
+    { name: '여름', age: 1 },
+  ];
+  const info = {
+    name: '홍길동',
+    phone: null, //null은 값이 없음을 의미
+  };
+
   return (
     <div>
+      {/* or연산자는 처음 만나는 참값을 출력 */}
+      {info.name}의 전화번호는 {info.phone || '번호가 없습니다.'}입니다.
+      <ul>
+        {/* {users.map((item, index) => {
+          const users = users[0];
+          <li key={index}>{`${item.name}이는${item.age}살입니다.`}</li>;
+        })} */}
+        {/* <li>{`${users[0].name}이는${users[0].age}살입니다.`}</li>
+        <li>{`${users[1].name}이는${users[0].age}살입니다.`}</li>
+        <li>{`${users[2].name}이는${users[0].age}살입니다.`}</li> */}
+      </ul>
+      <ul>
+        {/* map() 메서드를 사용해서 반복 */}
+        {array.map((item, index) => {
+          <li key={index}>{item}</li>;
+        })}
+
+        <li>{array[0]}</li>
+        <li>{array[1]}</li>
+        <li>{array[2]}</li>
+      </ul>
       {array[0]}, {array[1]}, {array[2]}
       {/* // 나의 배우자는 1명이고 자식은 2명이다 */}
       <hr />
